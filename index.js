ad2ac9bc9a9f3daaece1e93cd6ba19506efa4a48
mujav <48324559+mujav@users.noreply.github.com>
2021-11-10 19:46:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router(),
+UserRoutes = require('./user'),
+PostRoutes = require('./post')
+
+router.use('/users', UserRoutes)
+router.use('/posts', PostRoutes)
+
+module.exports = router
---
+++ user.js
@@ -0,0 +1,13 @@
+const router = require('express').Router(),
+UserController = require('../controllers/user')
+
+router.post('/login', UserController.authenticate)
+router.use(UserController.verifyJWT)
+router.get('/', UserController.index)
+router.get('/:uid', UserController.show)
+router.put('/:uid/update', UserController.update)
+router.delete('/:uid/delete', UserController.delete)
+router.post('/create', UserController.create)
+
+
+module.exports = router
